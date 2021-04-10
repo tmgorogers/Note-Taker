1c1a10082debbe46692217527370073c19928037
Tierra Rogers <tierragore@yahoo.com>
2021-04-10 14:56:24 -0400

    Editing files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,54 +1,62 @@
 //Importing File System
 //Utilites module
+//
 const router = require("express").Router();
 const notes = require("../db/db.json");
 const fs = require('fs');
+const util = require('util');
+const { v1 } = require('uuid');
 
-const readFileAsync = util.promisify(fs.readfile);
+//Convert fs.readFile/fs.writeFile into Promise version of same
+const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 async function readFile() {
     const data = await readFileAsync("db/db.json", "UTF-8");
     return JSON.parse(data);
 }
-router.get('/notes',
-    async (req, res) => {
+
+router.get('/notes', async (req, res) => {
         const data = await readFile();
         res.json(data);
-    });
+});
 
 
 //GET .api.notes- Should read the "db.json" file and return all saved notes as JSON.
 router.post('/notes', async function (req, res) {
     const data = await readFile();
-    console.log({ data, body: req.body });
     let note = {
-        id: data.length + 1,
+        id: v1(),
         title: req.body.title,
         text: req.body.text,
     };
+    data.push(note);
+    console.log(data)
+    const file = await writeFileAsync("db/db.json", JSON.stringify(data), "UTF-8");
+    res.json(data)
+});
 //delete route
-    router.delete('/notes/:id', async function (req, res) {
-        var data = await readFile();
-        let id = req.params.id;
+router.delete('/notes/:id', async function (req, res) {
+    var data = await readFile();
+    let id = req.params.id;
 
-        function deleteNote() {
-            data = data.filter((note) => note.id != id);
-            writeFileAsync("db/db.json", JSON.stringify(data), "UTF-8");
-            res.json(data);
-        }
-        deleteNote();
-    });
-    //updat route
-    router.put("/notes/:id", async function (req,res) {
-        var data = await readFile();
-        let id = req.params.id;
-        //newdata = date.filter (id);
-        let note = {
-            id: req.params.id,
-            title: req.body.title,
-            text: req.body.text,
-        };
-    });
-    
-    module.exports = router;
+    function deleteNote() {
+        data = data.filter((note) => note.id != id);
+        writeFileAsync("db/db.json", JSON.stringify(data), "UTF-8");
+        res.json(data);
+    }
+    deleteNote();
+});
+//updat route
+router.put("/notes/:id", async function (req, res) {
+    var data = await readFile();
+    let id = req.params.id;
+    //newdata = date.filter (id);
+    let note = {
+        id: req.params.id,
+        title: req.body.title,
+        text: req.body.text,
+    };
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // create application/urlencoded parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(_dirname + "/public"));
+app.use(express.static("public"));
 
 //Routes
 app.use("/api",apiRoutes);
